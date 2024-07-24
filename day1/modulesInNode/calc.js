53a604b28e2a45982f9ae921079a15f80e7f0f2f
ANAS <anaspappadan@gmail.com>
2024-07-24 11:32:58 +0530

    modules in node

---
+++ day1/modulesInNode/calc.js
@@ -0,0 +1,10 @@
+exports.add = function (a, b) {
+  return a + b;
+};
+
+// or
+function sum(a, b) {
+  return a + b;
+}
+
+exports.sum = sum;
---
+++ day1/modulesInNode/index.js
@@ -0,0 +1,4 @@
+const calc = require("./calc");
+
+console.log(calc.add(1, 7));
+console.log(calc.sum(4, 5));
