33ad19c6a7af01f529067708225b98cfaadddbaf
ANAS <anaspappadan@gmail.com>
2024-07-26 15:23:35 +0530

    module caching

---
+++ moduleCaching/index.js
@@ -0,0 +1,7 @@
+const superHero = require("./superhero");
+console.log(superHero.getName());
+superHero.setName("spiderman");
+console.log(superHero.getName());
+
+const newSupe = require("./superhero");
+console.log(newSupe.getName());
